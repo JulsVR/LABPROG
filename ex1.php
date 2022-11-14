a91a96dcdd60bc1220415ea71bae11b32c3ddae9
JulsVR <julesvr.official@gmail.com>
2022-11-14 17:42:27 +0000

    Second commit

--- ex1.php
+++ ex1.php
@@ -1,9 +1,11 @@
 <?php
-function even_or_odd($min, $max){
-            for($i = $min; $i <= $max; $i++){
-                if($i % 2 == 0){
+function evenOrOdd($min, $max)
+{
+            for ($i = $min; $i <= $max; $i++) {
+                if ($i % 2 == 0){
                    echo $i . " -- even" . "<br>";
-                }else{echo $i . " -- odd" . "<br>";}
+                }else {echo $i . " -- odd" . "<br>";}
             } echo "<br>";
         }
-?>
+
+        evenOrOdd(1, 10);
