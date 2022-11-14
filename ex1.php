558dcfed9c09ce08ad83cccfec9beac98f6a4e3f
JulsVR <julesvr.official@gmail.com>
2022-11-14 17:43:13 +0000

    Fix [3]

--- ex1.php
+++ ex1.php
@@ -2,7 +2,7 @@
 function evenOrOdd($min, $max)
 {
             for ($i = $min; $i <= $max; $i++) {
-                if ($i % 2 == 0){
+                if ($i % 2 == 0) {
                    echo $i . " -- even" . "<br>";
                 }else {echo $i . " -- odd" . "<br>";}
             } echo "<br>";
